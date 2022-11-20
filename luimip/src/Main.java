4c9d27e8605073f943f2e857abf468c1f767a768
luismip <luismiguelperez971@gmail.com>
2022-11-20 03:37:37 -0300

    modificado

--- luimip/src/Main.java
+++ luimip/src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int i=20;
+
+        // nuevo desarrollo
+        int modificado = 30;
+
     }
     
 }
